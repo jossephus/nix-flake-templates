4ca676bf4634985829d3068c1c0985d2aaa00685
jossephus <yosephtuemay64@gmail.com>
2024-03-19 01:04:20 +0300

    wip

--- flake.nix
+++ flake.nix
@@ -1,28 +1,12 @@
 {
   description = "Flako Tempo";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-
-    alejandra = {
-        url = "github:kamadorueda/alejandra/3.0.0";
-        inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    flake-utils = {
-      url = "github:numtide/flake-utils";
-    };
-  };
-
-  outputs = { nixpkgs, alejandra, flake-utils,  ... }: flake-utils.lib.eachDefaultSystem(system:
-     let pkgs = nixpkgs.legacyPackages.${system}; 
-     in {
+  outputs = self: rec {
        templates = {
          opengl = {
            path = ./opengl-glut;
            description = "OpenGl + Glut for Nix Setup";
          };
        };
-     }
-  );
+  };
 }
