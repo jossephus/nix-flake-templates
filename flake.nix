8c10ae59b680bcb1ab11c4f285e193dec4226dad
jossephus <yosephtuemay64@gmail.com>
2024-03-19 00:57:29 +0300

    add common flake nix

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Flako Tempo";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+    alejandra = {
+        url = "github:kamadorueda/alejandra/3.0.0";
+        inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+  };
+
+  outputs = { nixpkgs, alejandra, flake-utils,  ... }: flake-utils.lib.eachDefaultSystem(system:
+     let pkgs = nixpkgs.legacyPackages.${system}; 
+     in {
+       templates = {
+         opengl-glut = {
+           path = ./opengl-glut;
+           description = "OpenGl + Glut for Nix Setup";
+         };
+       };
+     }
+  );
+}
