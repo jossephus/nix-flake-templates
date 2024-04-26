2a4bcc0ba7a97afc15151bcf7492dd99689d0bda
jossephus <yosephtuemay64@gmail.com>
2024-04-26 16:12:04 +0300

    feat: add init folder

---
+++ init/flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Flake with zero stuff";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+         pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+          ];
+        };
+      });
+}
